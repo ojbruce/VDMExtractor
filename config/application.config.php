607de5bb96c8b05dc864ec81a91f7db74d278bb1
ojbruce <olivia.j.bruce@gmail.com>
2014-07-22 00:29:23 +0200

    Submit config files

---
+++ config/application.config.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'modules' => [
+        'Extractor',
+        'API',
+        'DoctrineModule',
+        'DoctrineORMModule',
+    ],
+    'module_listener_options' => [
+        'module_paths' => [
+            './module',
+            './vendor',
+        ],
+
+        'config_glob_paths' => [
+            'config/autoload/{,*.}{global,local}.php',
+        ],
+    ],
+];
